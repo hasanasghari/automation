ec24e51f594803a23608b5f3187e6eaafd7476a7
Hasan Asghari <52317411+hasanasghari@users.noreply.github.com>
2019-12-04 20:38:17 +0330

    add some help

--- netmiko/ssh-1.py
+++ netmiko/ssh-1.py
@@ -2,9 +2,9 @@ from netmiko import ConnectHandler
 
 IOSv={
 	'device_type' : 'cisco_ios',
-	'host':'192.168.122.102',
-	'username':'hasan',
-	'password':'hasan'
+	'host':'192.168.122.102', # put device ip here
+	'username':'hasan',# put username here
+	'password':'hasan' # put password here
 }
 
 Connect=ConnectHandler(**IOSv)
